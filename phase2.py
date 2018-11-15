40158203f0e2ade0efae82690f2fc67a04604ed8
Jesse Goertzen <jesse.goertzen2@gmail.com>
2018-11-14 21:06:04 -0700

    phase 2

--- phase2.py
+++ phase2.py
@@ -19,11 +19,30 @@ def boolInput(string, clear=True):
 
 def adsHash(name="./indexes/ad.idx"):
     database = db.DB()
-    database.open(name, None, db.DB_HASH, db.DB_CREATE)
+    database.open(name, None, db.DB_CREATE|db.DB_HASH)
     with open("./output/ads.txt", 'r') as f:
         for l in f:
             match = re.search("(\\d{10}):(.*)",l)
             key, data = match.group(1).encode(), match.group(2)
-            database.put(key, data, db.DB_KEYFIRST)
+            database.put(key, data)
+    database.close()
 
+def datesBtree(name="./indexes/da.idx"):
+    database = db.DB()
+    database.open(name, None, db.DB_CREATE|db.DB_BTREE)
+    with open("./output/pdates.txt", 'r') as f:
+        for l in f:
+            match = re.search("(\\d{4}/\\d{2}/\\d{2}):(.*)")
+            key, data = match.group(1).encode(), match.group(2)
+            database.put(key, data)
+    database.close()
 
+def pricesBtree(name="./indexes/pr.idx"):
+    database = db.DB()
+    database.open(name, None, db.DB_CREATE|db.DB_BTREE)
+    with open("./output/prices.txt", 'r') as f:
+        for l in f:
+            match = re.search("(\\d*):(.*)")
+            key, data = match.group(1).encode(), match.group(2)
+            database.put(key, data)
+    database.close()
