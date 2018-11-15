a47b868991cd912ec288a3de0887abf3925d7846
StickNStones <zkist@ualberta.ca>
2018-11-14 22:28:29 -0700

    Post-test

--- phase2.py
+++ phase2.py
@@ -3,40 +3,60 @@ import re
 
 def adsHash(name="./indexes/ad.idx"):
     database = db.DB()
-    database.open(name, None, db.DB_CREATE|db.DB_HASH)
+    database.open(name, None, db.DB_HASH, db.DB_CREATE)
     with open("./output/ads.txt", 'r') as f:
         for l in f:
-            match = re.search("(\\d{10}):(.*)",l)
+            match = re.search("([0-9a-zA-Z-_]+):(.*)",l)
             key, data = match.group(1).encode(), match.group(2)
             database.put(key, data)
     database.close()
 
 def datesBtree(name="./indexes/da.idx"):
     database = db.DB()
-    database.open(name, None, db.DB_CREATE|db.DB_BTREE)
-    with open("./output/pdates.txt", 'r') as f:
+    database.set_flags(db.DB_DUP)
+    database.open(name, None, db.DB_CREATE, db.DB_BTREE)
+    with open("./output/pdate.txt", 'r') as f:
         for l in f:
-            match = re.search("(\\d{4}/\\d{2}/\\d{2}):(.*)", l)
+            match = re.search("(\d{4}/\d{2}/\d{2}):(.*)", l)
             key, data = match.group(1).encode(), match.group(2)
             database.put(key, data)
     database.close()
 
 def pricesBtree(name="./indexes/pr.idx"):
     database = db.DB()
-    database.open(name, None, db.DB_CREATE|db.DB_BTREE)
+    database.set_flags(db.DB_DUP)
+    database.open(name, None, db.DB_CREATE, db.DB_BTREE)
     with open("./output/prices.txt", 'r') as f:
         for l in f:
-            match = re.search("(\\d*):(.*)", l)
+            match = re.search("(\d*):(.*)", l)
             key, data = match.group(1).encode(), match.group(2)
             database.put(key, data)
     database.close()
     
 def termsBtree(name="./indexes/te.idx"):
     database = db.DB()
-    database.open(name, None, db.DB_BTREE|db.DB_CREATE)
+    database.set_flags(db.DB_DUP)
+    database.open(name, None, db.DB_BTREE, db.DB_CREATE)
     with open("./output/terms.txt", 'r') as f:
         for l in f: 
-            match = re.search("(.*):(.*)", l)
+            match = re.search("([0-9a-zA-Z-_]+):(.*)", l)
             key, data = match.group(1).encode(), match.group(2)
             database.put(key, data)
     database.close()
+
+
+def init():
+    termsBtree("./indexes/te.idx")
+    pricesBtree()
+    datesBtree()
+    adsHash()
+
+#  Test to iterate and print all values that were put into the database. Put loop before database.close()
+#    curs = database.cursor()
+#    iterr = curs.first()
+#    while iterr:
+#        print(iterr)
+#        iterr = curs.next()
+#    curs.close()
+
+init()
