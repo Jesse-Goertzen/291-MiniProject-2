cbbb216e5a85fc8f17b7be58c1861ce33c374b7a
Jesse Goertzen <jesse.goertzen2@gmail.com>
2018-11-14 18:10:35 -0700

    prep and useless

--- phase2.py
+++ phase2.py
@@ -1,1 +1,18 @@
 import bsddb3
+
+# Dunno if we'll need this but I had the idea and wanted to make it just incase
+from time import sleep
+from os import system
+def boolInput(string, clear=True):
+    while True:
+        if clear:
+            system('cls||clear')
+        sel = input(string + '[y/n]\n\r').lower()
+        if sel == y:
+            return True
+        elif sel == n:
+            return False
+        else:
+            print("Invalid selection, try again.")
+            sleep(0.75)
+
---
+++ phase3.py
@@ -0,0 +1,4 @@
+import bsddb3
+import re
+
+# I have a feeling this is gonna require a lot of regex....
