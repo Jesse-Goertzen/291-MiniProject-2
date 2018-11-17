1fef5dbb94a3e75f339e2f9d2f9368c65870e2c4
StickNStones <35318606+StickNStones@users.noreply.github.com>
2018-11-17 16:11:25 -0700

    Create Main3.py

---
+++ Main3.py
@@ -0,0 +1,21 @@
+import bsddb3
+import re
+import Date
+from Parser import Parser
+
+# datebase.get(b'stuff')
+# only returns a value
+
+# curs.set(b'stuff')
+# returns the associated key/value pair
+
+# examples:
+# Starting_Name = input("Enter the Starting_Name: ")
+# result = curs.set_range(Starting_Name.encode("utf-8")) 
+
+# if (str(result[0].decode("utf-8")[0:len(Ending_Name)]) < Ending_Name):     check if name comes before ending_name
+
+def main():
+
+
+    
