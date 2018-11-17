d0be9acf8609ebff6fa9d28a62d24e20b38e4d7a
Jesse Goertzen <jesse.goertzen2@gmail.com>
2018-11-17 15:37:48 -0700

    queries

--- phase3.py
+++ phase3.py
@@ -5,43 +5,48 @@ import Date
 # I have a feeling this is gonna require a lot of regex....
 
 class Parser():
+    string = ""
+
     # returns: (operator {str}, date {date})
     # Use findall and have operator and date in one group, then do another regex to create a tuple for each date query 
-    def _dateQuery(self, string):
-        match = re.search("date\s*(<|<=|>|>=|=)\s*(\d{4}/\d{2}/\d{2})", string)
-        if match:
-            return (match.group(1), Date(match.group(2)))
-        else:
-            return None 
+    def _dateQuery(self):
+        match = re.findall("date\s*(<|<=|>|>=|=)\s*(\d{4}/\d{2}/\d{2})", self.string)
+        self.string = re.sub("date\s*(<|<=|>|>=|=)\s*\d{4}/\d{2}/\d{2}", "", self.string)
+        return match if match else None
+
 
     # returns: (operator {str}, price {int})
-    def _priceQuery(self, string):
-        match = re.search("price\s*(<|<=|>|>=|=)\s*(\d+)", string)
-        if match:
-            return (match.group(1), (int) match.group(2))
-        else:
-            return None
+    def _priceQuery(self):
+        match = re.findall("price\s*(<|<=|>|>=|=)\s*(\d+)", self.string)
+        self.string = re.sub("price\s*(<|<=|>|>=|=)\s*\d+", "", self.string)
+        return match if match else None
     
-    def _locationQuery(self, string):
-        match = re.search("location\s*=\s*([0-9a-zA-Z_-]+)", string)
-        if match:
-            return match.group(1).lower()
-        else:
-            return None
+    def _locationQuery(self):
+        match = re.search("location\s*=\s*([0-9a-zA-Z_-]+)", self.string)
+        self.string = re.sub("location\s*=\s*[0-9a-zA-Z_-]+", "", self.string)
+        return match.group(1).lower() if match else None
     
-    def _catQuery(self, string):
-        match = re.search("cat\s*=\s*([0-9a-zA-Z_-]+)", string)
-        if match:
-            return match.group(1).lower()
-        else:
-            return None
+    def _catQuery(self):
+        match = re.search("cat\s*=\s*([0-9a-zA-Z_-]+)", self.string)
+        self.string = re.sub("cat\s*=\s*[0-9a-zA-Z_-]+", "", self.string)
+        return match.group(1).lower() if match else None
 
     # Maybe have the other methods return the query with the sub query removed,
     # then after all other methods are called the only thing left is a term query?
-    def _termQuery(self, string):
-        #not implemented
-        return None
+    def _termQuery(self):
+        match = re.findall("[a-zA-Z0-9_-]*%?")
+        self.string = re.sub("[a-zA-Z0-9_-]*%?", "", self.string)
+        return match if match else None
 
-    def _subQueries(self, string):
+    def parse(self, string):
         queries = dict()
-        match = re.search("")
+        queries["date"] = self._dateQuery()
+        queries["prices"] = self._priceQuery()
+        queries["loc"] = self._locQuery()
+        queries["cat"] = self._catQuery()
+        queries["terms"] = self._termQuery()
+        for q in queries:
+            if queries[q] is None:
+                del queries[q]
+                
+        return queries
