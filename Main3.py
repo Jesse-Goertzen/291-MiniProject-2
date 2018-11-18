941fdb8fbc73aa3fec1622d0c3e36b0f5a11e719
StickNStones <zkist@ualberta.ca>
2018-11-17 19:51:12 -0700

    WIP

--- Main3.py
+++ Main3.py
@@ -20,25 +20,43 @@ def main():
         #query = input("Let me no whut your query is: ").lower()
         p = Parser()
         parsedq = p.parse(input("Let me no whut your query is: ").lower())
-        dbfile = "./indexes/pr.idx"
+        dbfile = "./indexes/te.idx"
         database = db.DB()
         #database.set_flags(db.DB_DUP)
         database.open(dbfile, None, db.DB_BTREE, db.DB_CREATE)
         curs = database.cursor()
-        print(parsedq["price"])
-       # curs = database.cursor()
-        iterr = curs.first()
-        while iterr:
-            print(iterr)
-            iterr = curs.next()
-        curs.close()
+        # line under must be changed from "thing" to whatver key you want such as "date"
+        print(parsedq["term"])
+       # curs = database.cursor() 
+       # aint nobody knows what happens beyond this point
+       # !WARNING! !WARNING!
+ #       iterr = curs.first()
+ #       while iterr:
+ #           print(iterr)
+ #           iterr = curs.next()
+ #       curs.close()
+ #       curs = database.cursor()
+ #       result = curs.set_range((parsedq["price"][1]).encode("utf-8"))
+ #       while result != None:
+ #           if int(result[0].decode("utf-8")) < int(parsedq["price"][1]):
+ #               break
+ #           print(result[0].decode("utf-8"))
+ #           result = curs.next()
+
+    #    curs = database.cursor()
+    #    print(type(parsedq["date"][0]))
+    #    result = curs.set_range(((str(parsedq["date"][0][1]).encode("utf-8"))))
+    #    while result != None:
+    #        if result[0].decode("utf-8") < (parsedq["date"][0][1]):
+    #            break
+    #        print(result[0].decode("utf-8"))
+    #        result = curs.next()
+
         curs = database.cursor()
-        result = curs.set_range((parsedq["price"][1]).encode("utf-8"))
+        result = curs.set_range((str(parsedq["term"][0])).encode("utf-8"))
         while result != None:
-            if int(result[0].decode("utf-8")) < int(parsedq["price"][1]):
+            if result[0].decode("utf-8") > (parsedq["term"][0]):
                 break
-            print(result[0].decode("utf-8"))
+            print(result[1].decode("utf-8"))
             result = curs.next()
-
-
 main()
