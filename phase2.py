fb520553072f5f80c915ba9eb9841d160e308beb
StickNStones <zkist@ualberta.ca>
2018-11-19 13:08:17 -0700

    Structy

--- phase2.py
+++ phase2.py
@@ -1,5 +1,6 @@
 from bsddb3 import db
 import re
+import struct
 
 def adsHash(name="./indexes/ad.idx"):
     database = db.DB()
@@ -26,17 +27,29 @@ def pricesBtree(name="./indexes/pr.idx"):
     database = db.DB()
     database.set_flags(db.DB_DUP)
     database.open(name, None, db.DB_CREATE, db.DB_BTREE)
+    curs = database.cursor()
     with open("./output/prices.txt", 'r') as f:
         for l in f:
             match = re.search("(\d*):(.*)", l)
-            key, data = match.group(1).encode(), match.group(2)
+            key, data = int(match.group(1)), match.group(2)
+            key = struct.pack('>l', key)
             database.put(key, data)
+
+    curs.close()
+    curs = database.cursor()
+    iterr = curs.first()
+    while iterr:
+       # print(iterr)
+        print(struct.unpack('>l', iterr[0]))
+        iterr = curs.next()
+    curs.close()
+
     database.close()
     
 def termsBtree(name="./indexes/te.idx"):
     database = db.DB()
     database.set_flags(db.DB_DUP)
-    database.open(name, None, db.DB_BTREE, db.DB_CREATE)
+    database.open(name, None, db.DB_CREATE, db.DB_BTREE)
     with open("./output/terms.txt", 'r') as f:
         for l in f: 
             match = re.search("([0-9a-zA-Z-_]+):(.*)", l)
@@ -46,7 +59,7 @@ def termsBtree(name="./indexes/te.idx"):
 
 
 def init():
-    termsBtree("./indexes/te.idx")
+    termsBtree()
     pricesBtree()
     datesBtree()
     adsHash()
