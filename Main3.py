2ebbe2b232ec0b1bcb06f216688348363cd77be3
StickNStones <zkist@ualberta.ca>
2018-11-19 21:30:59 -0700

    Prices '<', '>', '>=' values

--- Main3.py
+++ Main3.py
@@ -2,6 +2,7 @@ from bsddb3 import db
 import re
 import Date
 from Parser import Parser
+import struct
 
 # datebase.get(b'stuff')
 # only returns a value
@@ -20,13 +21,13 @@ def main():
         #query = input("Let me no whut your query is: ").lower()
         p = Parser()
         parsedq = p.parse(input("Let me no whut your query is: ").lower())
-        dbfile = "./indexes/te.idx"
+        dbfile = "./indexes/pr.idx"
         database = db.DB()
         #database.set_flags(db.DB_DUP)
-        database.open(dbfile, None, db.DB_BTREE, db.DB_CREATE)
+        database.open(dbfile, None, db.DB_BTREE)
         curs = database.cursor()
         # line under must be changed from "thing" to whatver key you want such as "date"
-        print(parsedq["term"])
+        print(parsedq["price"])
        # curs = database.cursor() 
        # aint nobody knows what happens beyond this point
        # !WARNING! !WARNING!
@@ -35,28 +36,58 @@ def main():
  #           print(iterr)
  #           iterr = curs.next()
  #       curs.close()
- #       curs = database.cursor()
- #       result = curs.set_range((parsedq["price"][1]).encode("utf-8"))
- #       while result != None:
- #           if int(result[0].decode("utf-8")) < int(parsedq["price"][1]):
- #               break
- #           print(result[0].decode("utf-8"))
- #           result = curs.next()
+        curs = database.cursor()
+        result = curs.set_range(struct.pack('>l', int(parsedq["price"][0][1])))
 
-    #    curs = database.cursor()
+        # testing for logic with inequalities
+        # first is the >, else is <. Each individually therefore would have their own while loops for when >=
+        if parsedq["price"][0][0][0] == ">":
+            
+            # loop is for when >= is the string, might not have to bother with making sure its an equal sign...
+            # while loops through all the same values
+            if (len(parsedq["price"][0][0]) > 1) and (parsedq["price"][0][0][1] == "="):
+                while struct.unpack('>l', result[0])[0] == int(parsedq["price"][0][1]):
+                    print(struct.unpack('>l', result[0])[0])
+                    result = curs.next()
+            while result != None:
+                # while loops through all the same values to make sure there are none that are equal
+                while struct.unpack('>l', result[0])[0] == int(parsedq["price"][0][1]):
+                    result = curs.next()
+            
+            # if int(result[0].decode("utf-8")) < int(parsedq["price"][0][1]):
+            #     break
+            # struct.unpack('>l', iterr[0])[0]
+            # struct.pack('>l', key)
+                print(struct.unpack('>l', result[0])[0])
+                result = curs.next()
+        else:
+            # issue, 100 pulls 130 as the first value. Could hard code solution but not gonna hit up notes tonight
+            # i thought it grabbed smallest or equal value, guess it does greater or equal? yolo
+            # also not copying the code down tonight but it works for less than! <
+            while result != None:
+                print(struct.unpack('>l', result[0])[0])
+                result = curs.prev()
+#    curs = database.cursor()
     #    print(type(parsedq["date"][0]))
     #    result = curs.set_range(((str(parsedq["date"][0][1]).encode("utf-8"))))
+    #    if len of pasredq '>=' is 2 then
+    #        while date == date:
+    #            if result[0].decode("utf-8") < (parsedq["date"][0][1]):
+    #                break
+    #            print(result[0].decode("utf-8"))
+    #            result = curs.next()   
     #    while result != None:
     #        if result[0].decode("utf-8") < (parsedq["date"][0][1]):
     #            break
     #        print(result[0].decode("utf-8"))
     #        result = curs.next()
 
-        curs = database.cursor()
-        result = curs.set_range((str(parsedq["term"][0])).encode("utf-8"))
-        while result != None:
-            if result[0].decode("utf-8") > (parsedq["term"][0]):
-                break
-            print(result[1].decode("utf-8"))
-            result = curs.next()
+
+ #       curs = database.cursor()
+ #       result = curs.set_range((str(parsedq["term"][0])).encode("utf-8"))
+ #       while result != None:
+            #if result[0].decode("utf-8") > (parsedq["term"][0]):
+            #    break
+ #           print(result[1].decode("utf-8"))
+  #          result = curs.next()
 main()
