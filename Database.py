17db2063e3e06b3ea39dbb0a3daa56f3ad5a4ebe
Jesse Goertzen <jesse.goertzen2@gmail.com>
2018-11-21 09:37:03 -0700

    term queries, date and price range queries

--- Database.py
+++ Database.py
@@ -58,6 +58,47 @@ class Database():
         dbase.close()
 
 
+    def _betweenDates(self):
+        dbfile = "./indexes/da.idx"
+        dbase = db.DB()
+        dbase.open(dbfile, None, db.DB_BTREE)
+        cur = dbase.cursor()
+
+        lower, upper = '', ''        
+        for d in self.queries['date']:
+            if d.split()[0] == '>=' or d.split()[0] == '>':
+                lower = d
+            else:
+                upper = d
+        
+        result = set()
+        lop, ldate = lower.split()
+        line = cur.set_range(struct.pack('>l', date))
+        while eval(self._getKey(line) + lower + 'and' + self._getKey(line) + upper):
+            aid, cat, loc = line[1].split(',')
+
+            # Add the aid to result set if no location or catagory is specified,
+            # or there are no catagories and the location matches
+            # or there are no locations and the catagory matches
+            # or both are specified and match
+            if self.numLoc == 0 and self.numCat == 0:
+                result.add(aid)
+            elif (self.numLoc > 0 and loc in self.queries['loc']) and self.numCat == 0:
+                result.add(aid)
+            elif (self.numCat > 0 and cat in self.queries['cat']) and self.numLoc == 0:
+                result.add(aid)
+            elif ((self.numCat > 0 and cat in self.queries['cat']) 
+                    and (self.numLoc > 0 and loc in self.queries['loc'])):
+                result.add(aid)
+            else:
+                pass
+            
+            line = cur.next()
+
+        self.results.append(result)
+        dbase.close()
+
+
     def _priceQuery(self):
         dbfile = "./indexes/pr.idx"
         dbase = db.DB()
@@ -67,7 +108,7 @@ class Database():
         for query in self.queries['price']:
             op, price = query.split()
             result = set()
-            line = cur.set_range(struct.pack('>l', price))
+            line = cur.set_range(struct.pack('>l', int(price)))
 
             # Get passed the ads with price equal to the desired price to be greater than
             if op == '>':
@@ -103,6 +144,70 @@ class Database():
 
         dbase.close()
 
+
+    def _betweenPrices(self):
+        dbfile = "./indexes/pr.idx"
+        dbase = db.DB()
+        dbase.open(dbfile, None, db.DB_BTREE)
+        cur = dbase.cursor()
+
+        lower, upper = '', ''
+        for p in self.queries['price']:
+            if p.split()[0] == '>=' or p.split()[0] == '>':
+                lower = p
+            else:
+                upper = p
+
+        result = set()
+        lop, lprice = lower.split()
+        line = cur.set_range(struct.pack(int(lprice)))
+        if lop == '>':
+            while self._getKey(line) == lprice:
+                line = cur.next()
+        
+        while eval(self._getKey(line) + lower + ' and ' + self._getKey(line) + upper):
+            aid, cat, loc = line[1].split(',')
+
+            # Add the aid to result set if no location or catagory is specified,
+            # or there are no catagories and the location matches
+            # or there are no locations and the catagory matches
+            # or both are specified and match
+            if self.numLoc == 0 and self.numCat == 0:
+                result.add(aid)
+            elif (self.numLoc > 0 and loc in self.queries['loc']) and self.numCat == 0:
+                result.add(aid)
+            elif (self.numCat > 0 and cat in self.queries['cat']) and self.numLoc == 0:
+                result.add(aid)
+            elif ((self.numCat > 0 and cat in self.queries['cat']) 
+                    and (self.numLoc > 0 and loc in self.queries['loc'])):
+                result.add(aid)
+            else:
+                pass
+            
+            line = cur.next()
+
+        self.results.append(result)
+        dbase.close()
+
+
+    def _termQuery(self):
+        dbfile = "./indexes/te.idx"
+        dbase = db.DB()
+        dbase.open(dbfile, None, db.DB_BTREE)
+        cur = dbase.cursor()
+
+        result = set()
+        end = cur.last()
+        line = cur.first()
+        while line != end:
+            if self._getKey(line) in self.queries['term']:
+                result.add(line[1])
+            line = cur.next()
+        
+        self.results.add(result)
+        dbase.close()
+        
+
     def _checkPrices(self):
         prices = self.queries['price']
         g = list()
@@ -135,6 +240,7 @@ class Database():
             corrected.append(l[0][1])
         if corrected:
             self.queries['price'] = corrected
+
 
     def _checkDates(self):
         dates = self.queries['date']
@@ -185,17 +291,30 @@ class Database():
         numDate     = len(self.queries['date'])
 
         if numPrice > 0 and numDate == 0:
-            self._priceQuery()
+            if numPrice == 2:
+                self._betweenPrices()
+            else:
+                self._priceQuery()
         elif numDate > 0 and numPrice == 0:
-            self._dateQuery()
+            if numDate == 2:
+                self._betweenDates()
+            else:
+                self._dateQuery()
         elif numDate > 0 and numPrice > 0:
-            self._dateQuery()
-            self._priceQuery()
+            if numDate == 2:
+                self._betweenDates
+            else:   
+                self._dateQuery()
+            if numPrice == 2:
+                self._betweenPrices()
+            else:
+                self._priceQuery()
         else:
-            self.queries['price'] = ['> 0']
+            self.queries['price'] = ['>= 0']
             self._priceQuery()
 
-        # TODO: Term queries
+        if queries['term']:
+            self._termQuery()
 
         return set.intersection(*self.results)
 
